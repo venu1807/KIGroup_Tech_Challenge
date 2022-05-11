f3f626e2c10bebc22bbc94614af523d7283a35b6
VENUGOPAL ACHHE <venugopal.achhe@rwth-aachen.de>
2022-05-11 20:49:48 +0200

    modified main.py file

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import json
+from datetime import datetime
+from time import sleep
+import random
 
 
-# Press the green button in the gutter to run the script.
+def generate_record():
+    return {
+        "datetime": datetime.now().isoformat(),
+        "sales": {
+            "quantity": random.randint(1, 10),
+            "total_price": abs(round(random.random(), 2) * 10)
+        },
+        "analytics": {
+            "clicks": random.randint(1, 10),
+            "impressions": random.randint(10, 20)
+        }
+    }
+
+
+def store_json_data(path, data):
+    with open(path, "w") as json_file:
+        json.dump(data, json_file)
+
+
+def create_missing_folders(directory):
+    import os
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def main():
+    records_list = []
+    counter = 0
+    while True:
+        records_list.append(generate_record())
+        if len(records_list) == 10:
+            path = os.path.join("data", "json_files", f"file_{counter}.json")
+            print(f"writing data to {path}")
+            store_json_data(path=path, data=records_list)
+            records_list = []
+            counter += 1
+            sleep(5)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    create_missing_folders(directory=os.path.join("data", "json_files"))
+    create_missing_folders(directory=os.path.join("data", "csv_files"))
+    create_missing_folders(directory=os.path.join("data", "parquet_files"))
+    main()
